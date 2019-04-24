b9a87c73cd930e7be38cef4c70d91e596f9017ed
Sandeep <46845112+sandydv7@users.noreply.github.com>
2019-04-24 21:59:26 +0530

    Update LRTF_Scheduling.c

--- LRTF_Scheduling.c
+++ LRTF_Scheduling.c
@@ -12,8 +12,7 @@ struct Process {
     int InitAT;
     int InitBT; 
     int WaitingTime; 
-    int TurnAroundTime; 
-    int CompletionTime; 
+    int TurnAroundTime;
 };
 
 // Used functions in this Program
@@ -32,7 +31,7 @@ int comparator(const void *p, const void *q)
 int inTotal = 0; 
 int fiTotal = 0;
 
-int main() 
+void main() 
 { 
 	
   	printf("\n\n Enter the Number of Processes ::");
@@ -52,11 +51,31 @@ int main()
     	printf("\n\n\tEnter the %dth Process ID::",var_i+1);
     	scanf("%d",&Prr_Arr[var_i].ProcessID); 
     	
+    	while(1){
+    		if(Prr_Arr[var_i].ProcessID<0){
+    		printf("\nProcess ID Can't be Negative.. Enter Again::");
+    		scanf("%d",&Prr_Arr[var_i].ProcessID); 
+			}
+			else{
+				break;
+			}
+		}
+    	
     	Prr_Arr[var_i].ArrivalTime =0;
         Prr_Arr[var_i].InitAT=Prr_Arr[var_i].ArrivalTime;	
         
         printf("\tEnter the %dth Proccess Burst Time::",var_i+1);
         scanf("%d",&Prr_Arr[var_i].BurstTime);
+        
+        while(1){
+    		if(Prr_Arr[var_i].BurstTime<0){
+    		printf("\nProcess Burst Time Can't be Negative.. Enter Again::");
+    		scanf("%d",&Prr_Arr[var_i].BurstTime); 
+			}
+			else{
+				break;
+			}
+		}
         Prr_Arr[var_i].InitBT = Prr_Arr[var_i].BurstTime; 
         fiTotal += Prr_Arr[var_i].BurstTime;
         
@@ -82,47 +101,53 @@ int main()
   
    	int size;
    	size=sizeof(Prr_Arr)/sizeof(Prr_Arr[0]);
+   	
     qsort((void*)Prr_Arr, size, sizeof(Prr_Arr[0]), comparator); 
+    
     ExcutionOFProcesses(Prr_Arr,number_of_processes); 
-    int totalWT = 0; 
-    int totalTAT = 0; 
+    
+    int TOT_WT = 0; 
+    int TOT_TAT = 0; 
     for (var_i = 0; var_i < number_of_processes; var_i++) { 
         
 		 
-        Prr_Arr[var_i].TurnAroundTime = Prr_Arr[var_i].CompletionTime - Prr_Arr[var_i].InitAT; 
+        Prr_Arr[var_i].TurnAroundTime = Prr_Arr[var_i].TurnAroundTime - Prr_Arr[var_i].InitAT; 
         Prr_Arr[var_i].WaitingTime = Prr_Arr[var_i].TurnAroundTime - Prr_Arr[var_i].InitBT; 
   
        
-        totalWT += Prr_Arr[var_i].WaitingTime; 
+        TOT_WT += Prr_Arr[var_i].WaitingTime; 
   
        
-        totalTAT += Prr_Arr[var_i].TurnAroundTime; 
+        TOT_TAT += Prr_Arr[var_i].TurnAroundTime; 
     } 
     
     printf ("\n\n\t\tResult of Execution ... \n\n"); 
   
     
-    printf("\t\tPNo\tProcessID\tArrivalTime\tBurstTime\tCompletionTime\tTurnAroundTime\tWaitingTime\n"); 
+    printf("\t\tPNo\tProcessID\tArrivalTime\tBurstTime\tWaitingTime\tTurnAroundTime\n"); 
   
     for (var_i = 0; var_i < number_of_processes; var_i++) 
 	{ 
         printf("\t\t%d",Prr_Arr[var_i].ProcessNo); 
+
         printf("\t%d", Prr_Arr[var_i].ProcessID);
+
         printf("\t\t%d",Prr_Arr[var_i].InitAT); 
+
         printf("\t\t%d\t\t",Prr_Arr[var_i].InitBT); 
         
-        printf("%d\t\t",Prr_Arr[var_i].CompletionTime);
-        printf("%d\t\t",Prr_Arr[var_i].TurnAroundTime); 
         printf("%d\t",Prr_Arr[var_i].WaitingTime ); 
+
+        printf("\t%d",Prr_Arr[var_i].TurnAroundTime);         
         printf("\n");
     } 
-  
     printf("\n");
-    printf("\n\n\t\tTotal TAT     ::  %d\n",totalTAT); 
-    printf("\t\tAverage TAT   ::  %f\n",(float)totalTAT / (number_of_processes)); 
-    printf("\t\tTotal WT      ::  %d\n",totalWT); 
-    printf("\t\tAverage WT    ::  %f\n",(float)totalWT /(number_of_processes)); 
-    return 0; 
+    printf("\n\n\t\tTotal WatingTime      ::  %d\n",TOT_WT); 
+    printf("\t\tAverage WatingTime    ::  %f\n",(float)TOT_WT /(number_of_processes));
+    printf("\t\tTotal TurnAroundTime     ::  %d\n",TOT_TAT); 
+    printf("\t\tAverage TurnAroundTime   ::  %f\n",(float)TOT_TAT / (number_of_processes)); 
+     
+    
 } 
 
 //defination of Used functions
@@ -171,7 +196,9 @@ int ExcutionOFProcesses(struct Process P[],int number_p)
         			P[j].ArrivalTime+=1;
 				}
 		}
+		
 		printf("\n\tPNo\tProcessID\tCurentTime\tRequiresBurst\n");
+		
 		for (k = 0; k < number_p; k++) 
 		{ 
         	printf("\t%d\t",P[k].ProcessNo); 
@@ -183,7 +210,7 @@ int ExcutionOFProcesses(struct Process P[],int number_p)
     	printf("\n"); 
         i++; 
         if (P[index].BurstTime == 0) { 
-            P[index].CompletionTime = inTotal; 
+            P[index].TurnAroundTime = inTotal; 
             printf("\t\tProcess P%d has been completed at %d",P[index].ProcessNo ,inTotal); 
         } 
         printf("\n"); 
